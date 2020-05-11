0582725bf6e29c9e217655ada6c006cc49d05982
Afro Tsukuyomi <afrotsukuyomi@gmail.com>
2020-05-11 01:26:17 -0400

    plotting

--- statisticsProjectI.py
+++ statisticsProjectI.py
@@ -20,6 +20,7 @@ describeTable.drop(columns = describeTable.columns, inplace = True)
 opinionsfi = data.groupby(['Especialidad','Libro', 'Opinión del 1 al 6']).size()
 #print(opinionsfi.index.levels[1].values)
 missingVal = {}
+especialtyBooks = {}
 fiColumns = ['Especialidad','Libro','Opinión del 1 al 6','fi'] 
 for col in fiColumns:
   missingVal[col] = []
@@ -31,17 +32,22 @@ for col in fiColumns:
 #E.G: { 'Libro' : [2,3,4], 'Opinión del 1 al 6': [1,1,2], 'fi': [0,0,0]
 
 for book in opinionsfi.index.levels[1].values:
+  especialty = data[data[fiColumns[1]] == book][fiColumns[0]].iloc[0]
+  if(not especialty in especialtyBooks):
+    especialtyBooks[especialty] = []
+  especialtyBooks[especialty].append(book)
   for val in np.arange(1,7):
     j = (data[fiColumns[1]] == book) & (data[fiColumns[2]] == val)
     if(not j.any()):
-      missingVal[fiColumns[0]].append(data[data[fiColumns[1]] == book]['Especialidad'].iloc[0])
+      missingVal[fiColumns[0]].append(data[data[fiColumns[1]] == book][fiColumns[0]].iloc[0])
       missingVal[fiColumns[1]].append(book)
       missingVal[fiColumns[2]].append(val)
       missingVal[fiColumns[3]].append(0)
 
-
 nonPresentVal = pd.DataFrame(missingVal, columns = fiColumns)
+
 opinionshi = opinionsfi / opinionsfi.groupby(['Especialidad','Libro']).sum()  * 100
+
 bookRatesDistributions = opinionsfi.to_frame('fi').reset_index()
 bookRatesDistributions = pd.concat([bookRatesDistributions,nonPresentVal]).sort_values(by=['Especialidad','Libro', 'Opinión del 1 al 6'])
 bookRatesDistributions.set_index(['Especialidad','Libro', 'Opinión del 1 al 6'], inplace = True)
@@ -54,14 +60,24 @@ bookRatesDistributions['Hi'] = opinionshi.groupby(['Especialidad','Libro']).cums
 bookRatesDistributions['hi'].fillna(0, inplace = True) 
 bookRatesDistributions['Fi'].fillna(0, inplace = True) 
 bookRatesDistributions['Hi'].fillna(0, inplace = True) 
-
 # join the general information
 finalTable = describeTable.join(bookRatesDistributions, how='inner')
-finalTable.sort_values(by=['Especialidad','Número de Valoraciones','Opinión del 1 al 6'], inplace = True, ascending = [True,False, True])
 finalTable.set_index(finalTable.index.reorder_levels([*dataIndexes, 'Opinión del 1 al 6']), inplace = True)
+finalTable.sort_values(by=['Especialidad','Número de Valoraciones','Opinión del 1 al 6'], inplace = True, ascending = [True,False, True])
 finalTable.to_excel("output.xlsx")  
 
-bins = np.arange(1,7)
-# plt.hist(bookARatings,bins)
-# plt.hist(bookBRatings,bins)
-# plt.show()
+fisByEspecialty = [[data[data['Libro'] == book]['Opinión del 1 al 6'].values  for book in especialtyBooks[especialty]] for especialty in especialtyBooks]
+labels = [[especialty, especialtyBooks[especialty]] for especialty in especialtyBooks]
+
+# [[data[data['Libro'] == book]['Opinión del 1 al 6'].values
+bins = np.arange(0, 6 + 1.5) - 0.5
+for i in range(len(especialtyBooks)):
+  fig, ax = plt.subplots()
+  _ = ax.hist(fisByEspecialty[i], bins, density = True, label = labels[i][1])
+  ax.set_xticks(bins + 0.5)
+  ax.legend(prop={'size': 10})
+  ax.set_title(labels[i][0])
+  plt.figure(i)
+  i+=1
+plt.close(0)
+plt.show()
